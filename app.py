7f17e04dbac6120bc83a434faf060b743651dea8
Raymond Ji <raymondji@berkeley.edu>
2020-07-11 12:48:42 -0700

    wip

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_caching import Cache
+from bwf import rankings
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
-@app.route('/')
-def main():
-    return "bob"
+@app.route('/<event>/<year>/<month>/<rows>', methods=['GET'])
+@cache.cached(timeout=60)
+def rank(event, year, month, rows):
+    output = rankings(event, year, month, rows)
+    return jsonify(output)  
 
 if __name__ == '__main__':
     app.run(debug=True)
--- bwf.py
+++ bwf.py
@@ -1,5 +1,6 @@
 import requests
 import bs4
+import json
 from bs4 import BeautifulSoup
 
 
@@ -21,71 +22,71 @@ class Player:
 
 def rankings(event, year, month, rows):
 
-    if event == "men singles":
+    if event == "men-singles":
         page = requests.get('https://bwfbadminton.com/rankings/2/bwf-world-rankings/6/men-s-singles/' + str(year) + '/' 
         + str(month) + '/?rows=' + str(rows) + '&page_no=1')
-    elif event == "women singles":
+    elif event == "women-singles":
         page = requests.get('https://bwfbadminton.com/rankings/2/bwf-world-rankings/7/women-s-singles/' + str(year) + '/' 
         + str(month) + '/?rows=' + str(rows) + '&page_no=1')
-    elif event == "men doubles":
+    elif event == "men-doubles":
         page = requests.get('https://bwfbadminton.com/rankings/2/bwf-world-rankings/8/men-s-doubles/' + str(year) + '/' 
         + str(month) + '/?rows=' + str(rows) + '&page_no=1')
-    elif event == "women doubles":
+    elif event == "women-doubles":
         page = requests.get('https://bwfbadminton.com/rankings/2/bwf-world-rankings/9/women-s-doubles/' + str(year) + '/' 
         + str(month) + '/?rows=' + str(rows) + '&page_no=1')
-    elif event == "mixed doubles":
+    elif event == "mixed-doubles":
         page = requests.get('https://bwfbadminton.com/rankings/2/bwf-world-rankings/10/mixed-doubles/' + str(year) + '/' 
         + str(month) + '/?rows=' + str(rows) + '&page_no=1')
+    else:
+        return ["Invalid Input"]
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+    try:
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-    class_to_ignore = ["tr-ranking-detail"]
+        class_to_ignore = ["tr-ranking-detail"]
 
-    l = soup.find_all("tr", class_=lambda x: x not in class_to_ignore)
-    l.pop(0)
+        l = soup.find_all("tr", class_=lambda x: x not in class_to_ignore)
+        l.pop(0)
 
-    playerList = []
+        playerList = []
 
-    for player in l:
-        tags = [t for t in list(player.children) if type(t) == bs4.element.Tag]
-        rank = list(tags[0].children)[0]
+        for player in l:
+            tags = [t for t in list(player.children) if type(t) == bs4.element.Tag]
+            rank = list(tags[0].children)[0]
 
-        country = list(list(list(tags[1].children)[1].children)[1].children)[0]
-        country = country[country.find('\t')+1 : country.find('\t')+4]
+            country = list(list(list(tags[1].children)[1].children)[1].children)[0]
+            country = country[country.find('\t')+1 : country.find('\t')+4]
 
-        name = list(list(tags[2].children)[1].children)
-        name.pop(0)
-        name.pop(len(name) - 1)
+            name = list(list(tags[2].children)[1].children)
+            name.pop(0)
+            name.pop(len(name) - 1)
 
-        if len(name) > 1:
-            name1 = list(list(name[0].children)[1].children)[0]
-            name2 = list(list(name[2].children)[1].children)[0]
-            name = name1 + "," + name2
-        else:
-            name = ' '.join(list(list(name[0].children)[1].children)[0].split())
+            if len(name) > 1:
+                name1 = list(list(name[0].children)[1].children)[0]
+                name2 = list(list(name[2].children)[1].children)[0]
+                name = name1 + "," + name2
+            else:
+                name = ' '.join(list(list(name[0].children)[1].children)[0].split())
 
-        ranking_change = list(tags[3].children)
-        ranking_num = int(list(ranking_change[1].children)[0])
-        if ranking_num != 0:
-            posneg = str(ranking_change[3])
-            index = posneg.find('arrow-up.png')
-            if index == -1:
-                ranking_num *= -1
+            ranking_change = list(tags[3].children)
+            ranking_num = int(list(ranking_change[1].children)[0])
+            if ranking_num != 0:
+                posneg = str(ranking_change[3])
+                index = posneg.find('arrow-up.png')
+                if index == -1:
+                    ranking_num *= -1
 
-        record = ''.join(list(tags[4].children)[0].split())
+            record = ''.join(list(tags[4].children)[0].split())
 
-        earnings = str(list(tags[5].children)[0])
+            earnings = str(list(tags[5].children)[0])
 
-        points = ''.join(list(list(tags[6].children)[1].children)[0].split())
-        playerList.append(Player(rank, country, name, ranking_num, record, earnings, points))
-    for p in playerList:
-        print(p)
-
-def profile():
+            points = ''.join(list(list(tags[6].children)[1].children)[0].split())
+            playerList.append(Player(rank, country, name, ranking_num, record, earnings, points))
+        return [p.__dict__ for p in playerList]
+    except:
+        return ["Invalid Input"]
     
     
-if __name__ == '__main__':
-    rankings("women singles", 2010, 12, 25)
 
 
 
