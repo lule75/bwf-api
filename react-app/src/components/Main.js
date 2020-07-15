6bd7b7186b8ade04a495d2da21e927b0ed361313
Raymond Ji <raymondji@berkeley.edu>
2020-07-15 00:29:27 -0700

    update

--- react-app/src/components/Main.js
+++ react-app/src/components/Main.js
@@ -2,25 +2,40 @@ import React, { Component } from 'react';
 import './Main.css';
 import Navbar from './navbar/Navbar';
 import Examples from './examples/Examples';
+import FadeIn from 'react-fade-in';
+
 
 class Main extends Component {
 
     render() {
         return (
-            <div id="outer">
-                <Navbar />
-                <div id="inner">
-                    <div className="intro">
-                        <p className="title">Up-to-date BWF World Rankings on demand.</p>
-                        <p className="description">Get current and historical badminton world rankings alongside player statistics with BWF API</p>
+            <FadeIn>
+                <div id="outer">
+                    <Navbar />
+                    <div id="inner">
+                        <div className="intro">
+                            <p className="title">Up-to-date BWF World Rankings on demand.</p>
+                            <p className="description">Get current and historical badminton world rankings alongside player statistics with BWF API</p>
+                        </div>
+                        <Examples />
                     </div>
-                    <Examples />
-                </div>
 
-                <div className="summary">
-                    <p><b>BWF API</b> is a simple and easy-to-use API that returns JSON metadata for current and historical <br/>badminton world rankings alongside detailed player information.</p>
+                    <div className="summary">
+                        <p className="blurb"><b>BWF API</b> is a simple and easy-to-use API that returns JSON metadata for current and historical <br/>badminton world rankings alongside detailed player information.</p>
+
+                        <div className="features">
+                            <p className="features-title">Fast</p>
+                            <p>Asynchronously cached for a fast response.</p>
+                        </div>
+
+                        <div className="features">
+                            <p className="features-title">Easy Integration</p>
+                            <p>Get JSON results with simple HTTP GET requests.</p>
+                        </div>
+
+                    </div>
                 </div>
-            </div>
+            </FadeIn>
         )
     }
 }
