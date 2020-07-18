806ada889ff3eece92559b66f57c0b058c219431
Raymond Ji <raymondji@berkeley.edu>
2020-07-18 00:34:43 -0700

    asdf

---
+++ build/precache-manifest.be20d03eeb321c14b730e02533212265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c224b8e7cbe6685c1bb491d38bf6be7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "333a57b256a4eddd8ce7",
+    "url": "/static/css/main.a92e50ac.chunk.css"
+  },
+  {
+    "revision": "8d6f7321d88d59ebb955",
+    "url": "/static/js/2.7f30a72f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7f30a72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "333a57b256a4eddd8ce7",
+    "url": "/static/js/main.4e6caaf9.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "0a7f26d51d4ac90dd240e581f31790d9",
+    "url": "/static/media/bwf.0a7f26d5.png"
+  }
+]);
