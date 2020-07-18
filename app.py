82843dd607cfce67a91fc7202feec46091fbf360
Raymond Ji <raymondji@berkeley.edu>
2020-07-18 00:49:44 -0700

    revert

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask_caching import Cache
 from bwf import rankings
 import datetime
 
-app = Flask(__name__, static_folder='./build', static_url_path='/')
+# app = Flask(__name__, static_folder='./build', static_url_path='/')
+app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
+# @app.route('/')
+# def index():
+#     return app.send_static_file('index.html')
 
 
 @app.route('/<event>/<year>/<week>/<rows>', methods=['GET'])
